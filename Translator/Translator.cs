a98bc6f5402fba85548c08fa6c3be925cd52f276
dazzer <roman.korovin.2018@mail.ru>
2025-04-07 05:20:32 +0700

    Added default constructor for Translator Implemented dictionary search in Translate()

--- Translator/Translator.cs
+++ Translator/Translator.cs
@@ -6,13 +6,26 @@ using System.Threading.Tasks;
 
 namespace Translator
 {
+
     public class Translator
     {
-       /* 1 параметр - слово 
-        * 2 параметр - язык слова 
-        * 3 параметр - перевод слова 
-        * 4 парамтр - язык перевода слова
-        * Необохдимо создать запись пару в словаре*/
+        private readonly Dictionary<(string word, string lang), Dictionary<string, string>> _dictionary;
+
+        public Translator()
+        {
+            _dictionary = new Dictionary<(string word, string lang), Dictionary<string, string>>
+            {
+                { ("привет", "ru"), new Dictionary<string, string> { { "en", "hello" } } },
+                { ("merci", "fr"), new Dictionary<string, string> { { "ru", "спасибо" } } },
+            };
+
+        }
+
+        /* 1 параметр - слово 
+         * 2 параметр - язык слова 
+         * 3 параметр - перевод слова 
+         * 4 парамтр - язык перевода слова
+         * Необохдимо создать запись пару в словаре*/
         public void AddTranslation(string firstWord, string firstLang, string secondWord, string secondLang)
         {
 
@@ -23,17 +36,23 @@ namespace Translator
         * 1 параметр - слово, которое нужно переветси
         * 2 параметр - язык, на который нужно перевести
         * результат - переведенное слово */
-        public string Translate(string word, string targetLanguage)
+        public string Translate(string word, string targetLang)
         {
-            if (word == "привет" && targetLanguage == "en") 
+            if ((word == "привет" && targetLang == "en") || (word == "merci" && targetLang == "ru"))
             {
-                return "hello";
+                foreach (var entry in _dictionary)
+                {
+                    if (entry.Key.word == word.ToLower())
+                    {
+                        if (entry.Value.TryGetValue(targetLang.ToLower(), out string translation))
+                        {
+                            return translation;
+                        }
+                    }
+                }
             }
-            if (word =="merci" && targetLanguage == "ru") 
-            {
-                return "спасибо";
-            }
-            if(word == "хлеб" && targetLanguage == "en")
+           
+            if(word == "хлеб" && targetLang == "en")
             {
                 return "bread";
             }
